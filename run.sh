155ce542b8e9a7654fee66e1621d679fbe8924a7
Zhu Li <zhu.andrew.li@gmail.com>
2017-02-20 21:13:06 -0700

    Add script for compiling and runing programs.

---
+++ run.sh
@@ -0,0 +1,11 @@
+echo "Naive version"
+gcc LogisticRegressionNaive.c -std=c11 -lm -o naive -mtune=generic -march=native -shared-libgcc -fno-tree-vectorize -O3 -Wall
+./naive
+
+echo "SSE version"
+gcc LogisticRegressionSSE.c -std=c11 -lm -o sse -mtune=generic -march=native -shared-libgcc -fno-tree-vectorize -O3 -Wall -msse
+./sse
+
+echo "AVX2 version"
+gcc LogisticRegressionAVX2.c -std=c11 -lm -o avx2 -mtune=generic -march=native -shared-libgcc -fno-tree-vectorize -O3 -Wall -mavx2
+./avx2
